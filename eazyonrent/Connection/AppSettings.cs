5e2fc93913be24a108af654f92538736fd45001b
rahul <rahulsinghalmumbai@gmail.com>
2025-08-29 13:25:31 +0530

    --

--- eazyonrent/Connection/AppSettings.cs
+++ eazyonrent/Connection/AppSettings.cs
@@ -1,9 +1,4 @@
-﻿using Java.Util.Zip;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
 namespace eazyonrent.Connection
 {
